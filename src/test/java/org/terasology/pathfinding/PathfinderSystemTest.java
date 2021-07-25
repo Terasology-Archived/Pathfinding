fb5340342e21107c768d0c11f08c7a20f7b99979
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-07-24 20:33:44 -0700

    test(PathfinderSystemTest): port to MTE

--- src/test/java/org/terasology/pathfinding/PathfinderSystemTest.java
+++ src/test/java/org/terasology/pathfinding/PathfinderSystemTest.java
@@ -2,18 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.pathfinding;
 
-import com.badlogic.gdx.physics.bullet.Bullet;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.joml.Vector3i;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.terasology.core.world.generator.AbstractBaseWorldGenerator;
-import org.terasology.engine.WorldProvidingHeadlessEnvironment;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.terasology.engine.core.ComponentSystemManager;
-import org.terasology.engine.core.SimpleUri;
-import org.terasology.engine.core.PathManager;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.entity.internal.PojoEntityManager;
@@ -21,6 +16,8 @@ import org.terasology.engine.entitySystem.event.internal.EventSystem;
 import org.terasology.engine.logic.characters.CharacterComponent;
 import org.terasology.engine.registry.CoreRegistry;
 import org.terasology.engine.world.chunks.event.OnChunkLoaded;
+import org.terasology.moduletestingenvironment.MTEExtension;
+import org.terasology.moduletestingenvironment.extension.Dependencies;
 import org.terasology.navgraph.NavGraphSystem;
 import org.terasology.pathfinding.componentSystem.PathfinderSystem;
 import org.terasology.pathfinding.model.Pathfinder;
@@ -31,19 +28,13 @@ import static org.mockito.Mockito.mock;
 /**
  * @author synopia
  */
+@ExtendWith(MTEExtension.class)
+@Dependencies("Pathfinding")
 public class PathfinderSystemTest {
     private PojoEntityManager entityManager;
     private EventSystem eventSystem;
     private NavGraphSystem navGraphSystem;
     private PathfinderSystem pathfinderSystem;
-    private WorldProvidingHeadlessEnvironment environment;
-
-    @BeforeEach
-    public void before() throws Exception {
-        // Hack to get natives to load for bullet
-        PathManager.getInstance().useDefaultHomePath();
-        Bullet.init();
-    }
 
     @Test
     public void updateChunkBeforePathRequests() throws InterruptedException {
@@ -85,20 +76,8 @@ public class PathfinderSystemTest {
         assertTrue(f3.isDone());
     }
 
-    @AfterEach
-    public void teardown() throws Exception {
-        environment.close();
-    }
-
     @BeforeEach
     public void setup() {
-        environment = new WorldProvidingHeadlessEnvironment();
-        environment.setupWorldProvider(new AbstractBaseWorldGenerator(new SimpleUri("")) {
-            @Override
-            public void initialize() {
-
-            }
-        });
         entityManager = (PojoEntityManager) CoreRegistry.get(EntityManager.class);
         eventSystem = CoreRegistry.get(EventSystem.class);
         navGraphSystem = new NavGraphSystem();
